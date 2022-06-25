2d6a70b6843d89519f034d03be14fc51c96864c1
20af02 <ammaarfirozi@gmail.com>
2022-06-25 00:20:40 -0400

    team colors
    added team color detection

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import os
-from turtle import color
-import matplotlib
-from Realsense.realsense_depth import *
-from Realsense.realsense import *
-from Algorithm.main import *
-import cv2
-import time
-import argparse
 from UART.uart import uart_server
+import argparse
+import time
+import cv2
+from Algorithm.main import *
+from Realsense.realsense import *
+from Realsense.realsense_depth import *
+import matplotlib
+from turtle import color
+import os
+import serial
+import numpy as np
+import struct
+opponent_team = 'r'  # 'r' or 'b'
 
 count = 0
 
@@ -15,23 +19,51 @@ matplotlib.use('TKAgg')
 # Disable tensorflow output
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import time
-import serial
-import numpy as np
-import struct
+
+red_lower = np.array([0, 43, 226])
+red_upper = np.array([60, 255, 255])
+blue_lower = np.array([68, 167, 131])
+blue_upper = np.array([105, 255, 255])
+
+
+def red_or_blue(color_frame):
+    hsv = cv2.cvtColor(color_frame, cv2.COLOR_BGR2HSV)
+    red_mask = cv2.inRange(hsv, red_lower, red_upper)
+    blue_mask = cv2.inRange(hsv, blue_lower, blue_upper)
+    _, red_contours, _ = cv2.findContours(
+        red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, blue_contours, _ = cv2.findContours(
+        blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(red_contours) > 0 and len(blue_contours) > 0:
+        r_area = 0
+        for c in red_contours:
+            r_area += cv2.contourArea(c)
+        for c in blue_contours:
+            b_area += cv2.contourArea(c)
+        if r_area > b_area:
+            return 'r'
+        else:
+            return 'b'
+    elif len(red_contours) > 0:
+        return 'r'
+
+    return 'b'
+
 
 def send_cords(ser, horiz_disp, vert_disp):
     global count
     data = struct.pack('fff', np.float32(
         horiz_disp/640), np.float32(vert_disp/480), 1)
-    #try:
+    # try:
     ser.write(data)
-    #except:
-        #print("Error writing serial data")
+    # except:
+    #print("Error writing serial data")
     count += 1
 
+
 def det_move_(obj_x_coord, obj_y_coord, xres, yres):
-    obj_y_coord=yres-obj_y_coord
+    obj_y_coord = yres-obj_y_coord
     centerx, centery = xres/2.0, yres/2.0
 
     move_x = obj_x_coord-centerx
@@ -65,13 +97,13 @@ def main(_argv):
     model = get_model()
 
     ser = serial.Serial(
-            port="/dev/ttyTHS1",
-            baudrate=115200,
-            bytesize=serial.EIGHTBITS,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            timeout=1
-        )
+        port="/dev/ttyTHS1",
+        baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        timeout=1
+    )
 
     # Initialize Algorithm
     oldCords = None
@@ -95,7 +127,7 @@ def main(_argv):
             try:
                 coordinates = get_coordinates(color_frame, model)
             except:
-                print("Error getting cordinates\n");
+                print("Error getting cordinates\n")
 
             if coordinates != None:
                 # Get Median Depth from depth frame
@@ -111,21 +143,22 @@ def main(_argv):
                     print(coordinates)
                     print(depth)
                     show_frame(color_frame, depth_frame, depth, coordinates)
-
+                if red_or_blue(color_frame) != opponent_team:
+                    pass
                # try:
-                final_cords=det_move_(
-                            (coordinates[0]+coordinates[2])/2,
-                            (coordinates[1]+coordinates[3])/2,
-                            640,
-                            480)
+                final_cords = det_move_(
+                    (coordinates[0]+coordinates[2])/2,
+                    (coordinates[1]+coordinates[3])/2,
+                    640,
+                    480)
                 print(final_cords[0], final_cords[1])
-                send_cords(ser, final_cords[0],final_cords[1])
+                send_cords(ser, final_cords[0], final_cords[1])
                # except:
-                 #   print("Error sending coordinates", coordinates[0], coordinates[1], coordinates[2], coordinates[3],  det_move_(
-                 #           (coordinates[0]+coordinates[2])/2,
-                 #           (coordinates[1]+coordinates[3])/2,
-                 #           640,
-                 #           480))
+                #   print("Error sending coordinates", coordinates[0], coordinates[1], coordinates[2], coordinates[3],  det_move_(
+                #           (coordinates[0]+coordinates[2])/2,
+                #           (coordinates[1]+coordinates[3])/2,
+                #           640,
+                #           480))
 
 
 if __name__ == '__main__':
